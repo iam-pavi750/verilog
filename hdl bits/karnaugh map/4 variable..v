dca2bdf406c5eba5d34d17c4a7747c3a39ecbcba
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-07-04 14:57:32 +0530

    Create 4 variable..v

---
+++ hdl bits/karnaugh map/4 variable..v
@@ -0,0 +1,27 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+    assign out = ~c&~d&b&~a|~c&~d&a&~b|~c&d&~a&~b|~c&d&a&b|c&d&~a&b|c&d&a&~b|c&~d&~a&~b|c&~d&a&b;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+endmodule
