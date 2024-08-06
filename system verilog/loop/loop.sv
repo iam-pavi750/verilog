95c37fdb67ef6b93d9a102fae5cdb9fce5df0344
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-08-06 15:11:20 +0530

    Create loop.sv

---
+++ system verilog/loop/loop.sv
@@ -0,0 +1,16 @@
+module example;
+  initial begin
+    for( int i=0;i<8;i++)
+      $display("value of i = %0d",i);
+  end
+  endmodule
+
+
+value of i = 0
+ value of i = 1
+ value of i = 2
+value of i = 3
+ value of i = 4
+value of i = 5
+value of i = 6
+ value of i = 7
