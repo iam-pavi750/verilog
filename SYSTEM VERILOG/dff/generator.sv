8809a2f03f5dbad3e0e2cea3145f9eba56375ee6
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-08-07 18:20:57 +0530

    Create generator.sv

---
+++ SYSTEM VERILOG/dff/generator.sv
@@ -0,0 +1,20 @@
+class generator;
+  transaction trans;
+  mailbox mail;
+  
+  function new(mailbox mail);
+    this.mail = mail ;
+  endfunction
+  
+  task gene;
+    
+    repeat(4)begin
+    trans = new();
+      trans.randomize();
+      mail.put(trans);
+      $display("trans is generator");
+      #5;
+    end
+  endtask
+endclass
+    
