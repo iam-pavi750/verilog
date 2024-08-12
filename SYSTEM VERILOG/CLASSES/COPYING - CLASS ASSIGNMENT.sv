10dd5e9568bda7f861bed1b72816c4f629954aea
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-08-12 22:41:52 +0530

    Create COPYING - CLASS ASSIGNMENT.sv

---
+++ SYSTEM VERILOG/CLASSES/COPYING - CLASS ASSIGNMENT.sv
@@ -0,0 +1,36 @@
+class mirafra;
+  int a;
+  int b;
+  
+  function new();
+    a=10;
+    b=20;
+  endfunction
+  
+  function void display();
+    $display( "ais %d and b is %d",a,b);
+  endfunction
+  
+endclass
+
+module tb;
+ mirafra tb1;
+  mirafra tb2;
+  initial begin 
+    tb1 = new();
+    tb1.display();
+    tb2=tb1;
+    tb2.display();
+    tb2.a=0;
+    tb2.b=1;
+    tb1.display();
+    tb2.display();
+  end
+endmodule
+
+
+ a is 10 and b is  20
+ a is 10 and b is  20
+ a is  0 and b is  1
+a is   0 and b is  1
+    
