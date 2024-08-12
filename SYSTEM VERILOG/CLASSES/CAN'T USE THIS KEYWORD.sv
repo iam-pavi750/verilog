272401662f2d96888f8fb5e1a238f7a152a70ca5
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-08-12 11:31:17 +0530

    Create CAN'T USE THIS KEYWORD.sv

---
+++ SYSTEM VERILOG/CLASSES/CAN'T USE THIS KEYWORD.sv
@@ -0,0 +1,30 @@
+class home;
+  
+ string fan = "on";
+  string switch = "on";
+  
+  function void open_electricity();
+    
+    string fan = "off";
+    string switch ="off";
+    fan=fan;
+    switch=switch;
+    $display(" fan is %s and switch is %s",fan,switch);
+  endfunction
+  
+endclass
+
+module tb;
+  
+  initial begin
+    home h1 ;
+    h1 = new();
+    h1.open_electricity;
+   
+    
+    $display("fan is %s and switch is %s",h1.fan,h1.switch);
+  end
+    endmodule
+
+fan is off and switch is off
+fan is on and switch is on
