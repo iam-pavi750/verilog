25935c1b340ef3967858eb1aa48b729f6b914b90
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-08-17 11:04:59 +0530

    Update PACKED.sv

--- SYSTEM VERILOG /ARRAY/STATIC/PACKED.sv
+++ SYSTEM VERILOG /ARRAY/STATIC/PACKED.sv
@@ -12,3 +12,36 @@ data is arr[3] = 0
 data is arr[2] = 0
 data is arr[1] = 1
 data is arr[0] = 0
+
+multipacked
+
+module packed_Array;
+  reg [2:0][3:0] abc;
+  
+  initial begin
+    abc = 4'b1010;
+    
+    foreach(abc[i])begin
+      foreach(abc[i][j])begin
+        $display("value of abc[%0d][%0d] = %0d",i,j,abc[i][j]);
+      end
+    end
+  end
+endmodule
+
+value of abc[2][3] = 0
+value of abc[2][2] = 0
+value of abc[2][1] = 0
+value of abc[2][0] = 0
+value of abc[1][3] = 0
+value of abc[1][2] = 0
+value of abc[1][1] = 0
+value of abc[1][0] = 0
+value of abc[0][3] = 1
+value of abc[0][2] = 0
+value of abc[0][1] = 1
+value of abc[0][0] = 0
+    
+
+
+        
