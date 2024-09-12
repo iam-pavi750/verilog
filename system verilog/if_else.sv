137194c51854c66d843f00ed1bbb91392f2f6f93
iam-pavi750 <110714667+iam-pavi750@users.noreply.github.com>
2024-09-12 11:33:08 +0530

    Create if_else.sv

---
+++ system verilog/if_else.sv
@@ -0,0 +1,17 @@
+module if_else;
+byte a;
+initial begin
+  a = -1;
+  $display ("Assign the value of a = %0d ",a);
+  $display("-------------------------------------");
+  if(a>-1)begin
+    $display ("Successfully enters into if block");
+    $display ("a is a postive number");
+    $display ("----------------------------------");
+  end 
+  else 
+    $display ("Number is negative");
+  $display ("Out of if else block");
+end 
+ 
+endmodule 
